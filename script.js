4a593b755c4236c30c81e6e84eb5163b5e646c5d
Jesus14 <163902270+Luchowww@users.noreply.github.com>
2026-06-25 19:26:34 -0500

    principio del proyecto

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+let computerScore = 0
+let humanScore = 0
+
+
+
+
+
+
+
+
+function getComputerChoice(){
+   let Random_num = getRandomInt(3)
+
+
+   if  (Random_num = 0){
+       
+   }else if (Random_num = 1){
+
+   }else if (Random_num = 2){
+
+   }
+}
+
+function getHumanChoice(){
+
+}
+
+function play_round(){
+
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
