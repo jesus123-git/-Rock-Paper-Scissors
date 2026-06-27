8a078546f68c8749ea7f87c5668510a3973d89bb
Jesus14 <195504852+jesus123-git@users.noreply.github.com>
2026-06-27 18:19:25 -0500

    piedra papel o tijera , falta la suma del score

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 
-
-
 let computerScore = 0
 let humanScore = 0
 
-
-
-
-
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 
 function getComputerChoice(){
-   let Random_num = getRandomInt(3)
-
-
-   if  (Random_num = 0){
-       
-   }else if (Random_num = 1){
-
-   }else if (Random_num = 2){
-
+   const random_num = getRandomInt(3)
+   if  (random_num == 0){
+      return "rock"
+   }else if (random_num == 1){
+      return "paper"
+   }else if (random_num == 2){
+      return "scissors"
    }
 }
 
+
 function getHumanChoice(){
+   let selection = prompt("Rock , Paper or scissors ? ").toLowerCase()
+   return selection
 
 }
 
-function play_round(){
-
+function playRound(computerSelection, humanSelection) {
+  if (computerSelection === humanSelection) {
+    alert("It's a draw");
+  } else if (
+    (computerSelection === "rock"     && humanSelection === "scissors") ||
+    (computerSelection === "paper"    && humanSelection === "rock")     ||
+    (computerSelection === "scissors" && humanSelection === "paper")
+  ) {
+    computerScore++;
+    alert("Machine won");
+  } else {
+    humanScore++;
+    alert("YOU won");
+  }
 }
-
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+
+
+playRound(humanSelection, computerSelection);   
